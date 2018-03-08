ac7924aa1d4c22e14e95ff245c678d17bca74e29
jbalcita <jbalcita@cj.com>
2018-03-07 17:01:58 -0800

    main module takes in a list of manager names

--- lib/jcbets_otp.ex
+++ lib/jcbets_otp.ex
@@ -1,11 +1,14 @@
 defmodule JcbetsOtp do
   alias JcbetsOtp.Worker
 
-  def hello do
-    {:ok, process} = Worker.start_link
-    ["jc", "lafer"] |> Enum.each(fn m -> Worker.get_record process, m end)
+  @spec get_all_time_record_for(list(String.t)) :: map
+  def get_all_time_record_for(managers) do
+    {:ok, process} = Worker.start_link()
+    managers |> Enum.each(fn m -> Worker.get_record process, m end)
     stats = Worker.get_stats(process)
     Worker.stop(process)
+
     stats
   end
+
 end
--- test/jcbets_otp_test.exs
+++ test/jcbets_otp_test.exs
@@ -2,7 +2,17 @@ defmodule JcbetsOtpTest do
   use ExUnit.Case
   doctest JcbetsOtp
 
-  test "greets the world" do
-    assert JcbetsOtp.hello() == %{"jc" => [118, 81, 0], "lafer" => [99, 113, 0]}
+  test "returns stats for the managers passed in" do
+    # given
+    managers = ["jc", "lafer"]
+
+    # when
+    actual = JcbetsOtp.get_all_time_record_for(managers)
+    expected = %{"jc" => [118, 81, 0], "lafer" => [99, 113, 0]}
+
+    # then
+    assert(expected == actual)
   end
+
+
 end
